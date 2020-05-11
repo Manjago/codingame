7b29b8fac36dccf95831a8add50ed59c2bf27558
Темненков Кирилл Дмитриевич <kirill.temnenkov@mts.ru>
2020-05-11 21:50:44 +0300

    //ver 2.2 improved dummy by histry

--- src/pacman.kt
+++ src/pacman.kt
@@ -1,4 +1,4 @@
-//ver 2.1
+//ver 2.2 improved dummy by histry
 import java.util.*
 import kotlin.math.abs
 
@@ -186,15 +186,24 @@ class Solver(private val width: Int, private val height: Int) {
         override fun isDummy() = ttl < 0
         override fun nextMove(pacman: Pacman): Move? {
             ttl--
-            return if (saved != null) {
-                saved
-            } else {
-                saved = randomMove(pacman)
-                randomMove(pacman)
+            if (saved == null) {
+                saved = moveFromHistry(pacman) ?: randomMove(pacman)
             }
+            return saved
         }
     }
 
+    private fun moveFromHistry(pacman: Pacman): Move? {
+        pelletsHistry.data.asReversed().forEach{hp ->
+            val pretender = hp.asSequence()
+                .sortedByDescending {  pacman.dist(it) }
+                .firstOrNull()
+            if (pretender != null) {
+                return@moveFromHistry Move(pacman, pretender)
+            }
+        }
+        return null
+    }
 
     inner class HarvesterStrategy : Strategy {
         override fun name() = "harv"
@@ -252,6 +261,8 @@ class Solver(private val width: Int, private val height: Int) {
         }
     }
 
+    private val pelletsHistry = LimitedDepot<Set<Pellet>>(10)
+
     fun nextMove(
         pacmans: List<Pacman>, pellets: Set<Pellet>, hisPacmans: List<Pacman>,
         turnNum: Int
@@ -262,6 +273,7 @@ class Solver(private val width: Int, private val height: Int) {
         this.myPacmans = pacmans
         this.hisPacmans = hisPacmans
         this.prevTurns = if (turnNum != 0) this.turns else mutableListOf()
+        pelletsHistry.push(pellets)
 
         removeDeads()
 
@@ -384,6 +396,16 @@ class Solver(private val width: Int, private val height: Int) {
                 kotlin.random.Random.nextInt(height)
             )
         )
+    }
+
+    class LimitedDepot<T>(private val limit: Int) {
+        val data = mutableListOf<T>()
+        fun push(item: T) {
+            data.add(item)
+            if (data.size > limit) {
+                data.removeAt(0)
+            }
+        }
     }
 
     companion object {
